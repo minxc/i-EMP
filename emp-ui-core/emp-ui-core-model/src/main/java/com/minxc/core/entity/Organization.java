89b8a124e53ab85724512d849a53c67705fa4f9c
闵现畅 <xianchangmin@126.com>
2018-07-25 16:48:44 +0800

    Update Organization.java

--- emp-ui-core/emp-ui-core-model/src/main/java/com/minxc/core/entity/Organization.java
+++ emp-ui-core/emp-ui-core-model/src/main/java/com/minxc/core/entity/Organization.java
@@ -3,6 +3,9 @@ package com.minxc.core.entity;
 import lombok.*;
 
 import javax.persistence.*;
+
+import com.google.common.collect.Sets;
+
 import java.io.Serializable;
 import java.util.Date;
 import java.util.HashSet;
@@ -28,15 +31,15 @@ public class Organization implements Serializable {
 
     private static final long serialVersionUID = 2246665191774203262L;
     @Id
-    @Column(name = "ORG_ID", length = 32)
+    @Column(name = "ORG_ID", length = 64)
     private String id; //主键
     @Column(name = "ORG_NAME", length = 64)
     private String name; //组织机构名称
-    @Column(name = "ORG_CODE", length = 32)
+    @Column(name = "ORG_CODE", length = 64)
     private String code; //组织结构编码
     @Column(name = "ORG_LEVEL")
     private int level; //组织结构层级
-    @Column(name = "ORG_PATH", length = 256)
+    @Column(name = "ORG_PATH", length = 255)
     private String path; //组织结构路径
 
     /**
@@ -49,6 +52,7 @@ public class Organization implements Serializable {
     /**
      * 子组织
      */
+    @Builder.Default
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "PARENT_OID",foreignKey = @ForeignKey(name="FK_ORG_PARENT_ID"))
     private Set<Organization> children = new HashSet<Organization>();
@@ -65,13 +69,17 @@ public class Organization implements Serializable {
     @Temporal(TemporalType.TIMESTAMP)
     private Date writeDate;
 
-    @Column(name = "CREATE_UID", length = 32)
+    @Column(name = "CREATE_UID", length = 64)
     private String createUid; //创建人
 
-    @Column(name = "WRITE_UID", length = 32)
+    @Column(name = "WRITE_UID", length = 64)
     private String writeUid; //上次更新人
 
-    @Column(name = "ORG_NOTES", length = 256)
+    @Column(name = "ORG_NOTES", length = 255)
     private String note; //组织结构备注
+    
+    @Builder.Default
+    @OneToMany(mappedBy = "organization", cascade = CascadeType.ALL)
+    private Set<UserOrganizationLink> userOrganizationLinks = Sets.newHashSet();
 
 }
