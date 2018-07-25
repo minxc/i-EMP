a53c64be6d4e571bca4da34818a937bcddbbeb65
min.xiachang <xianchangmin@126.com>
2018-07-25 08:25:47 +0800

    gengxin

--- emp-ui-core/emp-ui-core-app/src/main/java/com/minxc/EmpUIApplication.java
+++ emp-ui-core/emp-ui-core-app/src/main/java/com/minxc/EmpUIApplication.java
@@ -8,8 +8,16 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
  * 应用的入口层函数
  */
 @SpringBootApplication
-public class EmpUIApplication  extends SpringBootServletInitializer{
+//public class EmpUIApplication  extends SpringBootServletInitializer{
+//    public static void main(String[] args) {
+//        SpringApplication.run(EmpUIApplication.class, args);
+//    }
+//}
+
+
+public class EmpUIApplication {
     public static void main(String[] args) {
         SpringApplication.run(EmpUIApplication.class, args);
     }
 }
+
--- emp-ui-core/emp-ui-core-app/src/main/java/com/minxc/ui/controller/LoginController.java
+++ emp-ui-core/emp-ui-core-app/src/main/java/com/minxc/ui/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.minxc.ui.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @ClassName LoginController
@@ -16,12 +17,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class LoginController {
 
     @RequestMapping("/")
-    public String index(ModelMap map) {
-        // 加入一个属性，用来在模板中读取
-        map.addAttribute("name", "min");
-        map.addAttribute("bookTitle", "min");
-        // return模板文件的名称，对应src/main/resources/templates/welcome.html
-        return "login";
+    public ModelAndView login() {
+        ModelAndView mv = new ModelAndView("login");
+        return mv;
     }
 
 }
