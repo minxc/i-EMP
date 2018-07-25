b7f4746d453db3862ff6b856aaf7d52d0905f8ea
min.xiachang <xianchangmin@126.com>
2018-07-25 18:38:12 +0800

    修改项目

---
+++ emp-ui-core/emp-ui-core-rest/src/main/java/com/minxc/core/rest/MainController.java
@@ -0,0 +1,18 @@
+package com.minxc.core.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.xml.ws.RequestWrapper;
+
+/**
+ * @ClassName MainController
+ * @Description TODO
+ * @Author Xianchang.min
+ * @Date 2018/7/25 12:55
+ * @Version 1.0
+ **/
+@Controller
+@RequestMapping("/main")
+public class MainController {
+}
